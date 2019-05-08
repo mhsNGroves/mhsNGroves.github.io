f2a9c60d5f3a69727ab357af88518dd9083ba65e
mhsNGroves <48356553+mhsNGroves@users.noreply.github.com>
2019-05-08 08:57:52 -0400

    Update sketch.js

--- Using the mouse v.2/sketch.js
+++ Using the mouse v.2/sketch.js
@@ -1,3 +1,8 @@
+/*    Nicholas Groves
+     "Using the Mouse"
+     April 16th, 2019
+  Revised: May 8th, 2019    */
+
 
 function setup() {
   // put setup code here
@@ -7,23 +12,23 @@ function setup() {
 }
 function draw() {
   // put drawing code here
-doubleClicked();
+  doubleClicked();
 
 
 }
 
 function doubleClicked() {
 
-  if (mouseX > -10) {
+if (mouseX > -10) {
   stroke(random(255),random(255),random(255));
   strokeWeight(10);
-  line(mouseX,mouseY,width/2,height/2); /*this creates a line, that starts from the mouse coordinates
+  line(mouseX,mouseY,width/2,height/2); /*this creates a line, that starts from the mouse's coordinates
                                           and finishes in the center of the screen*/
 }
-  if (mouseX > -10) {
-    fill(random(255),random(255),random(255));
-    strokeWeight(1);
-    square(random(width),random(height),random(100),random(100)); /*this creates random sized square
+if (mouseX > -10) {
+  fill(random(255),random(255),random(255));
+  strokeWeight(1);
+  square(random(width),random(height),random(100),random(100)); /*this creates random sized square
                                                                     somewhere on the screen*/
 }
 }
