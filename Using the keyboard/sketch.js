3e94497fa47cbb39d5c7d18aa1c28465423abe68
mhsNGroves <48356553+mhsNGroves@users.noreply.github.com>
2019-05-08 09:05:02 -0400

    Update sketch.js

--- Using the keyboard/sketch.js
+++ Using the keyboard/sketch.js
@@ -1,3 +1,8 @@
+/*    Nicholas Groves
+   "Using The Keyboard"
+     April 24th, 2019
+  Revised: May 8th, 2019  */
+
 
 let x = 640;
 let y = 500;
@@ -9,36 +14,31 @@ function setup() {
 }
 function draw() {
   // put drawing code here
-
   ellipse(x,y,size,size);
-
 }
 function keyPressed() {
 
 if (keyCode === UP_ARROW) {
-    background(random(255),random(255),random(255));
-  y-=(random(100));//moves the shape up with a random value up to 100
+  background(random(255),random(255),random(255));
+  y-=(random(100));//moves the shape up a random value up to 100
   fill(random(255),random(255),random(255));
-
-  }
+}
 
 if (keyCode === LEFT_ARROW) {
-    background(random(255),random(255),random(255));
+  background(random(255),random(255),random(255));
   x-=(random(100));//same as line 20 but shape moves to the left
   fill(random(255),random(255),random(255));
-
 }
 
 if (keyCode === DOWN_ARROW) {
-    background(random(255),random(255),random(255));
+  background(random(255),random(255),random(255));
   y+=(random(100));//same as line 20 but shape moves down
   fill(random(255),random(255),random(255));
-
 }
 
 if (keyCode === RIGHT_ARROW) {
-    background(random(255),random(255),random(255));
+  background(random(255),random(255),random(255));
   x+=(random(100));//same as line 20 but shape moves to the right
   fill(random(255),random(255),random(255));
-}
+ }
 }
