ec1dbfb2152f3622dec8b2fd883fab40973304ac
mhsNGroves <48356553+mhsNGroves@users.noreply.github.com>
2019-05-08 08:45:48 -0400

    Update sketch.js

--- Face Assignment/sketch.js
+++ Face Assignment/sketch.js
@@ -1,3 +1,9 @@
+/* Nicholas, Zubair, Maki, and Kayden
+     "Face Assignment - Spongebob"
+            April 4th, 2019
+         Revised: May 8th, 2019   */
+
+
 //global variables
 
 let scale=1; // The scale value can be changed to change the size of Spongebob proportionally
@@ -92,66 +98,66 @@ function eyeCurve(arcX,arcY,arcW,arcH,arc1,arc2) {
 
 // Spongebob's dimples, made by Nick
 function drawDimples () {
- stroke(235,64,64);
- strokeWeight(1);
- fill(252,260,3);
- ellipse(width/2-eyeDiameter,height/2-eyeDiameter/2,dimpleWidth/1.42,dimpleWidth/1.66);
- noStroke();
- ellipse(width/2-eyeDiameter/0.8,height/2-eyeDiameter/2,dimpleWidth/2.5,dimpleWidth/2.5);
- fill(235,64,64);
- ellipse(width/2-eyeDiameter/1.05,height/2-eyeDiameter/2,dimpleWidth/12,dimpleWidth/12);
- ellipse(width/2-eyeDiameter/1.15,height/2-eyeDiameter/1.75,dimpleWidth/12,dimpleWidth/12);
- ellipse(width/2-eyeDiameter/0.95,height/2-eyeDiameter/1.75,dimpleWidth/12,dimpleWidth/12);
+  stroke(235,64,64);
+  strokeWeight(1);
+  fill(252,260,3);
+  ellipse(width/2-eyeDiameter,height/2-eyeDiameter/2,dimpleWidth/1.42,dimpleWidth/1.66);
+  noStroke();
+  ellipse(width/2-eyeDiameter/0.8,height/2-eyeDiameter/2,dimpleWidth/2.5,dimpleWidth/2.5);
+  fill(235,64,64);
+  ellipse(width/2-eyeDiameter/1.05,height/2-eyeDiameter/2,dimpleWidth/12,dimpleWidth/12);
+  ellipse(width/2-eyeDiameter/1.15,height/2-eyeDiameter/1.75,dimpleWidth/12,dimpleWidth/12);
+  ellipse(width/2-eyeDiameter/0.95,height/2-eyeDiameter/1.75,dimpleWidth/12,dimpleWidth/12);
 
- stroke(235,64,64);
- fill(252,260,3);
- ellipse(width/2+eyeDiameter,height/2-eyeDiameter/2,dimpleWidth/1.42,dimpleWidth/1.66);
- noStroke();
- ellipse(width/2+eyeDiameter/0.8,height/2-eyeDiameter/2,dimpleWidth/2.5,dimpleWidth/2.5);
- fill(235,64,64);
- ellipse(width/2+eyeDiameter/1.05,height/2-eyeDiameter/2,dimpleWidth/12,dimpleWidth/12);
- ellipse(width/2+eyeDiameter/1.15,height/2-eyeDiameter/1.75,dimpleWidth/12,dimpleWidth/12);
- ellipse(width/2+eyeDiameter/0.95,height/2-eyeDiameter/1.75,dimpleWidth/12,dimpleWidth/12);
+  stroke(235,64,64);
+  fill(252,260,3);
+  ellipse(width/2+eyeDiameter,height/2-eyeDiameter/2,dimpleWidth/1.42,dimpleWidth/1.66);
+  noStroke();
+  ellipse(width/2+eyeDiameter/0.8,height/2-eyeDiameter/2,dimpleWidth/2.5,dimpleWidth/2.5);
+  fill(235,64,64);
+  ellipse(width/2+eyeDiameter/1.05,height/2-eyeDiameter/2,dimpleWidth/12,dimpleWidth/12);
+  ellipse(width/2+eyeDiameter/1.15,height/2-eyeDiameter/1.75,dimpleWidth/12,dimpleWidth/12);
+  ellipse(width/2+eyeDiameter/0.95,height/2-eyeDiameter/1.75,dimpleWidth/12,dimpleWidth/12);
 
 
 }
 // Spongebob's nose made by Nick
 function drawNose () {
-  stroke(0);
-  strokeWeight(1);
-  fill(252,260,3);
-  ellipse(width/2,height/2-eyeDiameter/2,dimpleWidth/1.7,dimpleWidth/1.3);
-  noStroke();
-  fill(252,260,3);
-  ellipse(width/2-eyeDiameter/8,height/2-eyeDiameter/3,dimpleWidth/3,dimpleWidth/3);
+   stroke(0);
+   strokeWeight(1);
+   fill(252,260,3);
+   ellipse(width/2,height/2-eyeDiameter/2,dimpleWidth/1.7,dimpleWidth/1.3);
+   noStroke();
+   fill(252,260,3);
+   ellipse(width/2-eyeDiameter/8,height/2-eyeDiameter/3,dimpleWidth/3,dimpleWidth/3);
 }
 
 // Spongebob's teeth, made by Nick
 function drawTeeth () {
-fill(255);
-stroke(0);
-strokeWeight(1);
-rect(width/2-bodyWidth/18,height/2,bodyWidth/11,bodyWidth/8);
-rect(width/2+bodyWidth/18,height/2,bodyWidth/11,bodyWidth/8);
+   fill(255);
+   stroke(0);
+   strokeWeight(1);
+   rect(width/2-bodyWidth/18,height/2,bodyWidth/11,bodyWidth/8);
+   rect(width/2+bodyWidth/18,height/2,bodyWidth/11,bodyWidth/8);
 }
 
 // Spongebob's mouth, made by Nick
 function drawMouth () {
-  stroke(0);
-  strokeWeight(1);
-  fill(252,260,3);
-  arc(width/2,height/2-eyeDiameter/3.4,eyeDiameter*2,bodyWidth/6,2*PI,PI);
+   stroke(0);
+   strokeWeight(1);
+   fill(252,260,3);
+   arc(width/2,height/2-eyeDiameter/3.4,eyeDiameter*2,bodyWidth/6,2*PI,PI);
 
 }
 
 // Spongebob's chin, made by Nick
 function drawChin() {
 
-fill(252,260,3);
-stroke(235,64,64);
-strokeWeight(1);
-arc(width/2-eyeDiameter/4,height/2+eyeDiameter/2,eyeDiameter/2,eyeDiameter/4,2*PI,PI);
-arc(width/2+eyeDiameter/4,height/2+eyeDiameter/2,eyeDiameter/2,eyeDiameter/4,2*PI,PI);
+   fill(252,260,3);
+   stroke(235,64,64);
+   strokeWeight(1);
+   arc(width/2-eyeDiameter/4,height/2+eyeDiameter/2,eyeDiameter/2,eyeDiameter/4,2*PI,PI);
+   arc(width/2+eyeDiameter/4,height/2+eyeDiameter/2,eyeDiameter/2,eyeDiameter/4,2*PI,PI);
 
 }
 
@@ -159,35 +165,35 @@ arc(width/2+eyeDiameter/4,height/2+eyeDiameter/2,eyeDiameter/2,eyeDiameter/4,2*P
 // Spongebob's body made by Maki
 function sponge()
 {
-  stroke(153, 153, 0);
-  strokeWeight(8*scale);
-  fill(252,260,3);
+   stroke(153, 153, 0);
+   strokeWeight(8*scale);
+   fill(252,260,3);
 
   //Sponge 'bumps'
 
   // Left side
-  ellipse(width/2-bodyWidth/2.25,height/2+bodyWidth/3,eyeDiameter/1.5);
-  ellipse(width/2-bodyWidth/2.25,height/2+bodyWidth/5.25,eyeDiameter/1.5);
-  ellipse(width/2-bodyWidth/2.25,height/2+bodyWidth/24,eyeDiameter/1.5);
-  ellipse(width/2-bodyWidth/2.25,height/2-bodyWidth/10,eyeDiameter/1.5);
-  ellipse(width/2-bodyWidth/2.25,height/2-bodyWidth/4,eyeDiameter/1.5);
-  ellipse(width/2-bodyWidth/2.25,height/2-bodyWidth/2.5,eyeDiameter/1.5);
-  ellipse(width/2-bodyWidth/2.25,height/2-bodyWidth/1.9,eyeDiameter/1.5);
+   ellipse(width/2-bodyWidth/2.25,height/2+bodyWidth/3,eyeDiameter/1.5);
+   ellipse(width/2-bodyWidth/2.25,height/2+bodyWidth/5.25,eyeDiameter/1.5);
+   ellipse(width/2-bodyWidth/2.25,height/2+bodyWidth/24,eyeDiameter/1.5);
+   ellipse(width/2-bodyWidth/2.25,height/2-bodyWidth/10,eyeDiameter/1.5);
+   ellipse(width/2-bodyWidth/2.25,height/2-bodyWidth/4,eyeDiameter/1.5);
+   ellipse(width/2-bodyWidth/2.25,height/2-bodyWidth/2.5,eyeDiameter/1.5);
+   ellipse(width/2-bodyWidth/2.25,height/2-bodyWidth/1.9,eyeDiameter/1.5);
   // Right side
-  ellipse(width/2+bodyWidth/2.25,height/2+bodyWidth/3,eyeDiameter/1.5);
-  ellipse(width/2+bodyWidth/2.25,height/2+bodyWidth/5.25,eyeDiameter/1.5);
-  ellipse(width/2+bodyWidth/2.25,height/2+bodyWidth/24,eyeDiameter/1.5);
-  ellipse(width/2+bodyWidth/2.25,height/2-bodyWidth/10,eyeDiameter/1.5);
-  ellipse(width/2+bodyWidth/2.25,height/2-bodyWidth/4,eyeDiameter/1.5);
-  ellipse(width/2+bodyWidth/2.25,height/2-bodyWidth/2.5,eyeDiameter/1.5);
-  ellipse(width/2+bodyWidth/2.25,height/2-bodyWidth/1.9,eyeDiameter/1.5);
+   ellipse(width/2+bodyWidth/2.25,height/2+bodyWidth/3,eyeDiameter/1.5);
+   ellipse(width/2+bodyWidth/2.25,height/2+bodyWidth/5.25,eyeDiameter/1.5);
+   ellipse(width/2+bodyWidth/2.25,height/2+bodyWidth/24,eyeDiameter/1.5);
+   ellipse(width/2+bodyWidth/2.25,height/2-bodyWidth/10,eyeDiameter/1.5);
+   ellipse(width/2+bodyWidth/2.25,height/2-bodyWidth/4,eyeDiameter/1.5);
+   ellipse(width/2+bodyWidth/2.25,height/2-bodyWidth/2.5,eyeDiameter/1.5);
+   ellipse(width/2+bodyWidth/2.25,height/2-bodyWidth/1.9,eyeDiameter/1.5);
   // Top
-  ellipse(width/2-bodyWidth/3.15,height/2-bodyWidth/1.9,eyeDiameter/1.5);
-  ellipse(width/2-bodyWidth/5.25,height/2-bodyWidth/1.9,eyeDiameter/1.5);
-  ellipse(width/2-bodyWidth/17,height/2-bodyWidth/1.9,eyeDiameter/1.5);
-  ellipse(width/2+bodyWidth/14,height/2-bodyWidth/1.9,eyeDiameter/1.5);
-  ellipse(width/2+bodyWidth/5,height/2-bodyWidth/1.9,eyeDiameter/1.5);
-  ellipse(width/2+bodyWidth/3.125,height/2-bodyWidth/1.9,eyeDiameter/1.5);
+   ellipse(width/2-bodyWidth/3.15,height/2-bodyWidth/1.9,eyeDiameter/1.5);
+   ellipse(width/2-bodyWidth/5.25,height/2-bodyWidth/1.9,eyeDiameter/1.5);
+   ellipse(width/2-bodyWidth/17,height/2-bodyWidth/1.9,eyeDiameter/1.5);
+   ellipse(width/2+bodyWidth/14,height/2-bodyWidth/1.9,eyeDiameter/1.5);
+   ellipse(width/2+bodyWidth/5,height/2-bodyWidth/1.9,eyeDiameter/1.5);
+   ellipse(width/2+bodyWidth/3.125,height/2-bodyWidth/1.9,eyeDiameter/1.5);
 
 }
 
@@ -221,13 +227,13 @@ function drawPants() {
 
 // Function for drawing sponge holes, made by Maki
 function holes() {
-  noStroke();
-  fill(201, 190, 64);
-  ellipse(width/2-eyeDiameter*1.4,height/2-eyeDiameter*1.65, 40*scale, 50*scale);
-  ellipse(width/2-eyeDiameter*1.5,height/2-eyeDiameter*1.2, 25*scale, 30*scale);
-  ellipse(width/2+eyeDiameter*1.4,height/2-eyeDiameter*1.8, 30*scale, 35*scale);
-  ellipse(width/2-eyeDiameter*1.5,height/2+eyeDiameter/2, 25*scale, 35*scale);
-  ellipse(width/2-eyeDiameter*1.3,height/2+eyeDiameter, 40*scale, 50*scale);
-  ellipse(width/2+eyeDiameter*1.3,height/2+eyeDiameter/2, 40*scale, 50*scale);
-  ellipse(width/2+eyeDiameter,height/2+eyeDiameter, 25*scale, 35*scale);
+   noStroke();
+   fill(201, 190, 64);
+   ellipse(width/2-eyeDiameter*1.4,height/2-eyeDiameter*1.65, 40*scale, 50*scale);
+   ellipse(width/2-eyeDiameter*1.5,height/2-eyeDiameter*1.2, 25*scale, 30*scale);
+   ellipse(width/2+eyeDiameter*1.4,height/2-eyeDiameter*1.8, 30*scale, 35*scale);
+   ellipse(width/2-eyeDiameter*1.5,height/2+eyeDiameter/2, 25*scale, 35*scale);
+   ellipse(width/2-eyeDiameter*1.3,height/2+eyeDiameter, 40*scale, 50*scale);
+   ellipse(width/2+eyeDiameter*1.3,height/2+eyeDiameter/2, 40*scale, 50*scale);
+   ellipse(width/2+eyeDiameter,height/2+eyeDiameter, 25*scale, 35*scale);
 }
