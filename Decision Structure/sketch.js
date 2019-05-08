cb12c0a76f63650986bf88e109f57235809d0851
mhsNGroves <48356553+mhsNGroves@users.noreply.github.com>
2019-05-08 09:20:21 -0400

    Update sketch.js

--- Decision Structure/sketch.js
+++ Decision Structure/sketch.js
@@ -1,3 +1,9 @@
+/*     Nicholas Groves
+     "Decision Structure"
+        May 3rd, 2019
+    Revised: May 8th, 2019   */
+
+
 let choices = ['Rock','Paper','Scissors']
 let userChoice;
 let keyPress = false;
@@ -11,16 +17,16 @@ function setup() {
 
 function draw() {
   if (keyPress === false) {
-    fill(0);
-    textSize(40);
-    text('YOU',width/4-30,50);
-    fill(255,0,0);
-    textSize(80);
-    text('VS',width/2-50,70);
-    fill(0);
-    textSize(40);
-    text('CPU',950,50);
-    rect(width/2,0,5,900);
+  fill(0);
+  textSize(40);
+  text('YOU',width/4-30,50);
+  fill(255,0,0);
+  textSize(80);
+  text('VS',width/2-50,70);
+  fill(0);
+  textSize(40);
+  text('CPU',950,50);
+  rect(width/2,0,5,900);
 
   }
 
@@ -28,100 +34,100 @@ function draw() {
 
 function keyPressed(){
 /*------------------ROCK--------------------------*/
-  if (keyCode === 82) {
-    clear();
-    keyPress = true;
-    print("Rock!")
-    userChoice = random(choices)
-    if (userChoice === 'Rock') {
-    background(0,140,255);
-    textSize(40);
-    text('Rock',width/4-20,height/2);
-    text(userChoice,950,height/2);
-    textSize(50);
-    text('YOU TIED',width/2-45,height/2);
+if (keyCode === 82) {
+  clear();
+  keyPress = true;
+  print("Rock!")
+  userChoice = random(choices)
+  if (userChoice === 'Rock') {
+  background(0,140,255);
+  textSize(40);
+  text('Rock',width/4-20,height/2);
+  text(userChoice,950,height/2);
+  textSize(50);
+  text('YOU TIED',width/2-45,height/2);
     }//end if
-    else if (userChoice === 'Paper') {
-    background(255,0,0);
-    textSize(40);
-    text('Rock',width/4-20,height/2);
-    text(userChoice,950,height/2);
-    textSize(50);
-    text('YOU LOSE',width/2-45,height/2);
+else if (userChoice === 'Paper') {
+  background(255,0,0);
+  textSize(40);
+  text('Rock',width/4-20,height/2);
+  text(userChoice,950,height/2);
+  textSize(50);
+  text('YOU LOSE',width/2-45,height/2);
     }//end if
-    else if (userChoice === 'Scissors') {
-    background(0,255,0);
-    textSize(40);
-    text('Rock',width/4-20,height/2);
-    text(userChoice,950,height/2);
-    textSize(50);
-    text('YOU WIN',width/2-45,height/2);
-    }//end if
-  }//end key if
+else if (userChoice === 'Scissors') {
+  background(0,255,0);
+  textSize(40);
+  text('Rock',width/4-20,height/2);
+  text(userChoice,950,height/2);
+  textSize(50);
+  text('YOU WIN',width/2-45,height/2);
+  }//end if
+}//end key if
 
   /*------------------PAPER--------------------------*/
-    if (keyCode === 80) {
-      clear();
-      keyPress = true;
-      print("Paper!")
-      userChoice = random(choices)
-      if (userChoice === 'Rock') {
-      background(0,255,0);
-      textSize(40);
-      text('Paper',width/4-20,height/2);
-      text(userChoice,950,height/2);
-      textSize(50);
-      text('YOU WIN',width/2-45,height/2);
+if (keyCode === 80) {
+  clear();
+  keyPress = true;
+  print("Paper!")
+  userChoice = random(choices)
+if (userChoice === 'Rock') {
+  background(0,255,0);
+  textSize(40);
+  text('Paper',width/4-20,height/2);
+  text(userChoice,950,height/2);
+  textSize(50);
+  text('YOU WIN',width/2-45,height/2);
     }//end if
-      else if (userChoice === 'Paper') {
-      background(0,140,255);
-      textSize(40);
-      text('Paper',width/4-20,height/2);
-      text(userChoice,950,height/2);
-      textSize(50);
-      text('YOU TIED',width/2-45,height/2);
+else if (userChoice === 'Paper') {
+  background(0,140,255);
+  textSize(40);
+  text('Paper',width/4-20,height/2);
+  text(userChoice,950,height/2);
+  textSize(50);
+  text('YOU TIED',width/2-45,height/2);
     }//end if
-      else if (userChoice === 'Scissors') {
-      background(255,0,0);
-      textSize(40);
-      text('Paper',width/4-20,height/2);
-      text(userChoice,950,height/2);
-      textSize(50);
-      text('YOU LOSE',width/2-45,height/2);
+else if (userChoice === 'Scissors') {
+  background(255,0,0);
+  textSize(40);
+  text('Paper',width/4-20,height/2);
+  text(userChoice,950,height/2);
+  textSize(50);
+  text('YOU LOSE',width/2-45,height/2);
       }//end if
     }//end key if
 
     /*------------------SCISSORS--------------------------*/
-      if (keyCode === 83) {
-        clear();
-        keyPress = true;
-        print("Scissors!")
-        userChoice = random(choices)
-        if (userChoice === 'Rock') {
-        background(255,0,0);
-        textSize(40);
-        text('Scissors',width/4-20,height/2);
-        text(userChoice,950,height/2);
-        textSize(50);
-        text('YOU LOSE',width/2-45,height/2);
-        }//end if
-        else if (userChoice === 'Paper') {
-        background(0,255,0);
-        textSize(40);
-        text('Scissors',width/4-20,height/2);
-        text(userChoice,950,height/2);
-        textSize(50);
-        text('YOU WIN',width/2-45,height/2);
-        }//end if
-        else if (userChoice === 'Scissors') {
-        background(0,140,255);
-        textSize(40);
-        text('Scissors',width/4-20,height/2);
-        text(userChoice,950,height/2);
-        textSize(50);
-        text('YOU TIED',width/2-45,height/2);
-        }//end if
-      }//end key if
+if (keyCode === 83) {
+  clear();
+  keyPress = true;
+  print("Scissors!")
+  userChoice = random(choices)
+if (userChoice === 'Rock') {
+  background(255,0,0);
+  textSize(40);
+  text('Scissors',width/4-20,height/2);
+  text(userChoice,950,height/2);
+  textSize(50);
+  text('YOU LOSE',width/2-45,height/2);
+    }//end if
+else if (userChoice === 'Paper') {
+  background(0,255,0);
+  textSize(40);
+  text('Scissors',width/4-20,height/2);
+  text(userChoice,950,height/2);
+  textSize(50);
+  text('YOU WIN',width/2-45,height/2);
+    }//end if
+else if (userChoice === 'Scissors') {
+  background(0,140,255);
+  textSize(40);
+  text('Scissors',width/4-20,height/2);
+  text(userChoice,950,height/2);
+  textSize(50);
+  text('YOU TIED',width/2-45,height/2);
+    }//end if
+   }//end key if
 
 
 }//end function
