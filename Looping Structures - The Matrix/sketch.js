71ec4140e6f7822d68de99f586e1ae3800dea214
mhsNGroves <48356553+mhsNGroves@users.noreply.github.com>
2019-05-08 09:25:32 -0400

    Update sketch.js

--- Looping Structures - The Matrix/sketch.js
+++ Looping Structures - The Matrix/sketch.js
@@ -1,3 +1,9 @@
+/*       Nicholas Groves
+   "Looping Structures - The Matrix"
+          May 7th, 2019
+      Revised: May 8th, 2019    */
+
+//Global Variables
 let x = 20
 let y = 100
 let size = 50
@@ -9,7 +15,6 @@ function setup() {
   background(255);
 }
 
-
 function draw() {
 
   var x = 0
@@ -17,8 +22,8 @@ function draw() {
   for(var x = 0; x <= width; x = x + 50) {
   noStroke();
   fill(0,random(255),0);
-  ellipse(x,random(windowHeight),size,size);
+  ellipse(x,random(windowHeight),size,size);  // draws ellipses everywhere on the screen in columns that are 50 x-values apart
   textSize(30);
-  text(char(random(255)),random(windowWidth),random(windowHeight));
-}
+  text(char(random(255)),random(windowWidth),random(windowHeight));  // this draws random characters everywhere on the screen
+ }
 }
